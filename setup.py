9ea2cce16ba7a06c0c60ac5309d78f2aaf4a7895
adrianprochaska <adrian_prochaska@web.de>
2024-01-13 21:50:47 +0100

    initialization with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository for the Driven Data SNOMED CT Entity Linking Challenge (https://www.drivendata.org/competitions/258/competition-snomed-ct/)',
+    author='adrianprochaska',
+    license='MIT',
+)
